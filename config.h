b169d9f91b2e6196b29be1accc1d8cf12b56c517
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2022-08-16 18:59:25 -0600

    improvement: keybinding.

--- config.h
+++ config.h
@@ -87,7 +87,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_p,      spawn,          SHCMD("rofi -show combi") },
-	{ MODKEY,                       XK_x,      spawn,          SHCMD("rofi -show calc") },
+	{ MODKEY,                       XK_c,      spawn,          SHCMD("rofi -show calc") },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
