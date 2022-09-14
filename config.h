8f5af8883a3ed2c62d6c0a6446f57c3606a2c087
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2022-09-14 10:49:23 -0600

    chore:

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const char *colors[][3]      = {
     /*                      fg         bg        border   */
     [SchemeNorm]    = { col_white,  col_black,  col_red   },
     [SchemeSel]     = { col_white,  col_black,  col_green },
-	[SchemeStatus]  = { col_white,  col_black,  NULL  },
+    [SchemeStatus]  = { col_white,  col_black,  NULL  },
 };
 
 
