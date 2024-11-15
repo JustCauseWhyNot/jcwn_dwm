3441131e3094a50bb085d21ec1c8a9f28b6781c5
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2024-11-14 19:53:30 -0700

    improvement: swallow behavior change

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int swterminheritfs    = 0;        /* 1 terminal inherits fullscreen on unswallow, 0 otherwise */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static const int swterminheritfs    = 1;        /* 1 terminal inherits fullscreen on unswallow, 0 otherwise */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[] = {
