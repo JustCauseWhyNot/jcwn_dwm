1083b5250c0e3f530937f3387d3272d3e9a1c2a2
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2024-12-09 22:55:18 -0700

    fix: make previous ci work?

--- config.h
+++ config.h
@@ -57,7 +57,7 @@ static const Rule rules[] = {
 	{ "Firefox",  NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
 	{ "st-direct",NULL,       NULL,            0,         0,          1,           0,        -1 },
 	{ NULL,       NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
-	{ "pavucontrol-qt",NULL,  NULL,            0,           -1,           -1,      0,         0 },
+	{ "pavucontrol-qt",NULL,  NULL,            0,            0,           -1,      0,         0 },
 	{ "Steam",    NULL,       NULL,            0,            0,           -1,      0,         0,      1 },
 	{ "steam_app",NULL,       NULL,            0,            0,           -1,      0,         0,      1 },
 };
