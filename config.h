6b6c045e338f94622a18a1b2ab6fa96e2a1ef479
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2025-01-16 21:48:42 -0700

    improvement: formating

--- config.h
+++ config.h
@@ -52,12 +52,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title            tags mask  isfloating  isterminal  noswallow  monitor isgame */
+	/* class      instance    title            tags mask  isfloating  isterminal  noswallow  monitor  isgame */
 	{ "Gimp",     NULL,       NULL,            0,         1,          0,           0,        -1 },
 	{ "Firefox",  NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
 	{ "st-direct",NULL,       NULL,            0,         0,          1,           0,        -1 },
-       { "pavucontrol-qt", NULL,  NULL,            0,         0,          0,           0,         0 },
-	{ NULL,       NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
+	{ "pavucontrol-qt",NULL,  NULL,            0,         0,          0,           0,         0 },
+	{ NULL,       NULL,       "Event Tester",  0,         0,          0,           1,        -1 },    /* xev */
 	{ "Steam",    NULL,       NULL,            0,            0,           -1,      0,         0,      1 },
 	{ "steam_app",NULL,       NULL,            0,            0,           -1,      0,         0,      1 },
 };
