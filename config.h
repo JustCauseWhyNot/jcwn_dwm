bd5793649cd4006f679ff3bb39c6a8ca5e8a5af8
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2025-02-14 15:47:31 -0700

    improvement: gimp don't float

--- config.h
+++ config.h
@@ -53,7 +53,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title            tags mask  isfloating  isterminal  noswallow  monitor  isgame */
-	{ "Gimp",     NULL,       NULL,            0,         1,          0,           0,        -1 },
+	{ "Gimp",     NULL,       NULL,            0,         0,          0,           0,        -1 },
 	{ "Firefox",  NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
 	{ "kdeconnect-sms", NULL, NULL,            1 << 5,    0,          0,           0,        -1 },
 	{ "st-direct",NULL,       NULL,            0,         0,          1,           0,        -1 },
