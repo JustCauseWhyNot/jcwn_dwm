53d75486634e45a2a4afbf7284ac5094abfcd7cd
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2025-03-23 12:33:52 -0600

    improvement: make obs spawn tag 5

--- config.h
+++ config.h
@@ -57,6 +57,7 @@ static const Rule rules[] = {
 	{ "LibreWolf",		NULL,			NULL,		0,		0,		0,		-1,		-1 },
 	{ "Gimp",		NULL,			NULL,		0,		0,		0,		0,		-1 },
 	{ "kdeconnect.sms",	NULL,			NULL,		1 << 4,		0,		0,		0,		-1 },
+	{ "obs",		NULL,			NULL,		1 << 4,		0,		0,		0,		-1 },
 	{ "st-direct",		NULL,			NULL,		0,		0,		1,		0,		-1 },
 	{ "pavucontrol-qt",	NULL,			NULL,		0,		0,		0,		0,		-1 },
 	{ NULL,			NULL,			"Event Tester",	0,		0,		0,		1,		-1 },    /* xev */
