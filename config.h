89f1c4b55cc1b232d2ef403ad244f94b17a51974
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2025-04-02 23:01:15 -0600

    fix: keybind conflict with wt.

--- config.h
+++ config.h
@@ -136,7 +136,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscreen, {0} },
 	{ MODKEY|Mod1Mask,              XK_f,      togglefakefullscreen, {0} },
