28dc058d39b1514e4a87fb235c40e84f81ec8554
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2025-06-01 14:32:59 -0600

    feat: start qbittorrent in tag 6

--- config.h
+++ config.h
@@ -58,6 +58,7 @@ static const Rule rules[] = {
 	{ "Gimp",		NULL,			NULL,		0,		0,		0,		0,		-1 },
 	{ "kdeconnect.sms",	NULL,			NULL,		1 << 4,		0,		0,		0,		-1 },
 	{ "obs",		NULL,			NULL,		1 << 4,		0,		0,		0,		-1 },
+	{ "qbittorrent",	NULL,			NULL,		1 << 5,		0,		0,		0,		-1 },
 	{ "st-direct",		NULL,			NULL,		0,		0,		1,		0,		-1 },
 	{ "pavucontrol-qt",	NULL,			NULL,		0,		0,		0,		0,		-1 },
 	{ NULL,			NULL,			"Event Tester",	0,		0,		0,		1,		-1 },    /* xev */
